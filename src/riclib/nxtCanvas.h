b8ddc3aeb5d08376a22a2ac6326d3659cd0c07e3
Sebastian Wahl spillerrec <spillerrec@hotmail.com>
2010-11-08 00:17:35 +0000

    Changed the brush to a pixel class, a brush will simply just a pixel now. Also changed ricImage to nxtCanvas. The difference is that nxtCanvas will contain the drawing methods instead of having them in ricObject.
    git-svn-id: svn://svn.code.sf.net/p/riccreator/code@5 dab27aee-19fe-a441-870f-52ce2490c35b

---
+++ src/riclib/nxtCanvas.h
@@ -0,0 +1,51 @@
+/*
+	nxtCanvas class
+	
+	A nxtCanvas object provides a 2 dimentional drawing space.
+	A point has a X and Y value and defines the position in the drawing space. A point at (0,0) is positioned at the lower-left corner of the canvas. A positive X cordinate will move the point to the right and a positive Y value will more the point to the top. TODO: correct definition!
+	Each point has the properties of a ricPixel.
+	
+	The following drawing methods are supported:
+		Point
+		Line
+		Rectangle
+		Polygon*
+		Circle
+		Ellipse*
+		Text
+		Number
+*/
+#include "ricPixel.h"
+
+
+class nxtCanvas{
+	private:
+		unsigned int width;
+		unsigned int height;
+		ricPixel* map;
+		
+	public:
+		ricPixel brush;
+		
+		nxtCanvas(){
+			width = 0;
+			height = 0;
+			map = NULL;
+		}
+		
+		void create(unsigned int width, unsigned int height){
+			this->width = width;
+			this->height = height;
+			
+			if(map)
+				delete[] map;
+			map = new ricPixel [width*height];
+		}
+		
+		ricPixel* GetPixel(unsigned int X, unsigned int Y);
+		
+		~nxtCanvas(){
+			if(map)
+				delete[] map;
+		}
+};
---
+++ src/riclib/ricPixel.h
@@ -0,0 +1,51 @@
+/*
+	ricPixel class
+	
+	A ricPixel object contains the color of a pixel and how it interacts with a underlaying pixel.
+	The information stored is as following:
+		1 bit color, which reprecent a display's on/off state
+		1 bit alpha which enables transparentcy
+		The possibility to invert the color???	TODO: Check the neccesarily of this...
+		
+		4 different inteactions with an underlaying ricPixel: (will only have effect if the ricPixel is not transparent)
+			Copy: the underlaying ricPixel will be ignored completely
+			AND: an AND operation will be done on the two pixels and the result will be the new pixels color.
+			OR: same as AND, except that an OR operation will be done
+			XOR: same as AND, except that an XOR operation will be done
+			
+*/
+
+const char PIXEL_COLOR_WHITE = 0;
+const char PIXEL_COLOR_BLACK = 1;
+
+const char PIXEL_SOLID = 1;
+const char PIXEL_TRANSPARENT = 0;
+
+const char PIXEL_NO_INVERT = 0;
+const char PIXEL_INVERT = 1;
+
+const char PIXEL_MERGE_COPY = 0;
+const char PIXEL_MERGE_AND = 1;
+const char PIXEL_MERGE_OR = 2;
+const char PIXEL_MERGE_XOR = 3;
+
+
+class ricPixel{
+	private:
+		unsigned char bin;
+	
+	public:
+		Brush(){ bin = 0; }
+		
+		char GetColor(){ return (bin & 0x01); }
+		char GetAlpha(){ return (bin & 0x02) >> 1; }
+		char GetInvert(){ return (bin & 0x04) >> 2; }
+		char GetMerge(){ return (bin & 0x18) >> 3; }
+		
+		void SetColor(char color){ bin = (bin & 0xFC) | color; }
+		void SetAlpha(char alpha){ bin = (bin & 0xFD) | alpha; }
+		void SetInvert(char invert){ bin = (bin & 0xFB) | invert; }
+		void SetMerge(char merge){ bin = (bin & 0xE7) | merge; }
+		
+		void Merge(ricPixel* top);	//Places another rixPixel on top of this one and merges the result into this ricPixel
+};
