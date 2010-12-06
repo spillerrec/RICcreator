537907fc460165afa9eb89ff38d85acc43b860d0
Sebastian Wahl spillerrec <spillerrec@hotmail.com>
2010-12-06 15:28:53 +0000

    Cleanup in ricObjectWrite.cpp and fixing warnings
    git-svn-id: svn://svn.code.sf.net/p/riccreator/code@14 dab27aee-19fe-a441-870f-52ce2490c35b

--- src/riclib/ricObjectWrite.cpp
+++ src/riclib/ricObjectWrite.cpp
@@ -2,67 +2,78 @@
 
 int ricfile::ricOpOptions::write(ofstream* file){
 	write_header(file);
-	write_word(file, options);
-	write_word(file, width);
-	write_word(file, height);
+	options.write(file);
+	width.write(file);
+	height.write(file);
+	return 0;
 }
 
 
 int ricfile::ricOpSprite::write(ofstream* file){
 	write_header(file);
-	write_word(file, sprite_ID);
-	write_word(file, rows);
-	write_word(file, columns);
+	sprite_ID.write(file);
+	rows.write(file);
+	columns.write(file);
 	
 	file->write( image, rows * columns );
 	char temp[1];
 	temp[0] = 0;
 	if( (rows * columns) % 2 )
 		file->write( temp, 1 );
+	
+	return 0;
 }
 
 
 int ricfile::ricOpVarMap::write(ofstream* file){
 	write_header(file);
-	write_word(file, VarMapID);
-	write_word(file, size);
+	VarMapID.write(file);
+	size.write(file);
 	
-	for(int i=0; i<VarMap.size(); i++){
-		write_word(file, VarMap[i].X);
-		write_word(file, VarMap[i].Y);
+	for(unsigned int i=0; i<VarMap.size(); i++){
+		VarMap[i].X.write(file);
+		VarMap[i].Y.write(file);
 	}
+	
+	return 0;
 }
 
 
 int ricfile::ricOpCopyBits::write(ofstream* file){
 	write_header(file);
-	write_word(file, CopyOptions);
-	write_word(file, SpriteID);
-	write_word(file, posX);
-	write_word(file, posY);
-	write_word(file, width);
-	write_word(file, height);
-	write_word(file, relX);
-	write_word(file, relY);
+	CopyOptions.write(file);
+	SpriteID.write(file);
+	posX.write(file);
+	posY.write(file);
+	width.write(file);
+	height.write(file);
+	relX.write(file);
+	relY.write(file);
+	
+	return 0;
 }
 
 
 int ricfile::ricOpPixel::write(ofstream* file){
 	write_header(file);
-	write_word(file, CopyOptions);
-	write_word(file, posX);
-	write_word(file, posY);
-	write_word(file, value);
+	CopyOptions.write(file);
+	posX.write(file);
+	posY.write(file);
+	value.write(file);
+	
+	return 0;
 }
 
 
 int ricfile::ricOpLine::write(ofstream* file){
 	write_header(file);
-	write_word(file, CopyOptions);
-	write_word(file, startX);
-	write_word(file, startY);
-	write_word(file, endX);
-	write_word(file, endY);
+	CopyOptions.write(file);
+	startX.write(file);
+	startY.write(file);
+	endX.write(file);
+	endY.write(file);
+	
+	return 0;
 }
 
 
@@ -73,23 +84,29 @@ int ricfile::ricOpRectangle::write(ofstream* file){
 	write_word(file, posY);
 	write_word(file, width);
 	write_word(file, height);
+	
+	return 0;
 }
 
 
 int ricfile::ricOpCicle::write(ofstream* file){
 	write_header(file);
-	write_word(file, CopyOptions);
-	write_word(file, posX);
-	write_word(file, posY);
-	write_word(file, radius);
+	CopyOptions.write(file);
+	posX.write(file);
+	posY.write(file);
+	radius.write(file);
+	
+	return 0;
 }
 
 
 int ricfile::ricOpNumber::write(ofstream* file){
 	write_header(file);
-	write_word(file, CopyOptions);
-	write_word(file, posX);
-	write_word(file, posY);
-	write_word(file, number);
+	CopyOptions.write(file);
+	posX.write(file);
+	posY.write(file);
+	number.write(file);
+	
+	return 0;
 }
 
