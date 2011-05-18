68eaf987ce04e93a665c90e48238c587dd85cb91
Sebastian Wahl spillerrec <spillerrec@hotmail.com>
2011-05-18 21:24:58 +0000

    Added license stuff in every code file as supposed... Changed the about window a little bit.
    git-svn-id: svn://svn.code.sf.net/p/riccreator/code@55 dab27aee-19fe-a441-870f-52ce2490c35b

--- src/GUI/ricobjectview/ricobject_point.cpp
+++ src/GUI/ricobjectview/ricobject_point.cpp
@@ -1,3 +1,20 @@
+/*
+	This file is part of RICcreator.
+
+	RICcreator is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	RICcreator is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with RICcreator.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "ricobject_point.h"
 
 ricobject_point::ricobject_point( QWidget *parent ):
